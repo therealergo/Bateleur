68c0342220edfe4d9f67621ccdb752c98af0b434
Emilio Lopez <eil11@case.edu>
2019-03-11 10:51:58 -0500

    Starting out on GUI

--- src/main/com/bateleur/app/App.java
+++ src/main/com/bateleur/app/App.java
@@ -10,10 +10,10 @@ public class App extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-//        Parent root = FXMLLoader.load(getClass().getResource("/gui/views/sample.views"));
-//        primaryStage.setTitle("Hello World");
-//        primaryStage.setScene(new Scene(root, 300, 275));
-//        primaryStage.show();
+        Parent root = FXMLLoader.load(getClass().getResource("/gui/views/sample.fxml"));
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.show();
     }
 
     public static void main(String[] args) {
