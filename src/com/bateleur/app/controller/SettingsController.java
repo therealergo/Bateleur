3b9516c8df55b3eb625583a261d0432b83cbc1bd
therealergo <therealergo@gmail.com>
2019-03-20 04:13:32 -0400

    Fix syntax of skeleton implementation

--- src/com/bateleur/app/controller/SettingsController.java
+++ src/com/bateleur/app/controller/SettingsController.java
@@ -5,12 +5,9 @@ import com.bateleur.app.model.SettingsModel;
 import java.io.Serializable;
 
 public class SettingsController {
-
     public SettingsController(SettingsModel settings) {
-
     }
 
-    public void onSettingsChanged <T implements Serializable> (String key, Class<T> type, T value) {
-
+    public <T extends Serializable> void onSettingChanged (String key, Class<T> type, T value) {
     }
 }
