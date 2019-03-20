56e1d3297f6e42e454a910f223dc92df80a8b5ed
therealergo <therealergo@gmail.com>
2019-03-19 20:25:25 -0400

    Corrected Iterator implementation.

--- src/com/bateleur/app/model/LibraryModel.java
+++ src/com/bateleur/app/model/LibraryModel.java
@@ -1,52 +1,40 @@
 package com.bateleur.app.model;
 
-import com.bateleur.app.datatype.BAudio;
-
 import java.util.Comparator;
 import java.util.Iterator;
-import java.util.Spliterator;
-import java.util.function.Consumer;
 import java.util.function.Function;
 
-public class LibraryModel implements Iterable {
+import com.bateleur.app.datatype.BAudio;
+import com.therealergo.main.resource.ResourceFile;
+import com.therealergo.main.resource.ResourceFolder;
 
-    public LibraryModel (ResourceFile file, ResourceFolder data) {
+public class LibraryModel implements Iterable<BAudio> {
+	public LibraryModel (ResourceFile file, ResourceFolder data) {
 
-    }
+	}
 
-    public void update (SettingsModel settings) {
+	public void update (SettingsModel settings) {
 
-    }
+	}
 
-    public void sortBy(Comparator<BAudio> comparator) {
+	public void sortBy(Comparator<BAudio> comparator) {
 
-    }
+	}
 
-    public void filterBy(Function<BAudio, Boolean> filter) {
+	public void filterBy(Function<BAudio, Boolean> filter) {
 
-    }
+	}
 
-    public int size() {
+	public int size() {
 
-    }
+	}
 
-    public void reset() {
+	public void reset() {
 
-    }
+	}
 
-
-    @Override
-    public Iterator iterator() {
-        return null;
-    }
-
-    @Override
-    public void forEach(Consumer action) {
-
-    }
-
-    @Override
-    public Spliterator spliterator() {
-        return null;
-    }
+	@Override
+	public Iterator<BAudio> iterator() {
+		return null;
+	}
 }
