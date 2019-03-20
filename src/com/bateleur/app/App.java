e902025a63b136c7c07d72e101b6ab7b4ef1ddab
therealergo <therealergo@gmail.com>
2019-03-19 22:05:49 -0400

    Initial implementation of parts of LibraryModel

--- src/com/bateleur/app/App.java
+++ src/com/bateleur/app/App.java
@@ -1,7 +1,10 @@
 package com.bateleur.app;
 
+import java.util.Iterator;
+
 import com.bateleur.app.datatype.BAudio;
 import com.bateleur.app.datatype.BAudioFile;
+import com.bateleur.app.model.LibraryModel;
 import com.therealergo.main.Main;
 
 import javafx.application.Application;
@@ -20,15 +23,32 @@ public class App extends Application {
         primaryStage.show();
         
         { // Test of BAudio
-        	BAudio audio = new BAudioFile(Main.resource.getResourceFileLocal("test>meta_test_file"));
+        	BAudio audio = new BAudioFile(Main.resource.getResourceFileLocal("testBAudio>meta_test_file"));
 		    
 		    Integer test_meta0 = audio.<Integer>getMetadata("test_meta");
 		    Main.log.log(test_meta0);
 		    
-		    audio.<Integer>setMetadata("test_meta", 1235);
+		    audio.<Integer>setMetadata("test_meta", 123);
 		    
 		    Integer test_meta1 = audio.<Integer>getMetadata("test_meta");
 		    Main.log.log(test_meta1);
+        }
+        
+        { // Test of LibraryModel
+        	for (int i = 0; i<8; i++) {
+            	BAudio audio = new BAudioFile(Main.resource.getResourceFileLocal("testLibraryModel>meta_test_" + i));
+    		    audio.<Integer>setMetadata("test_meta", 1230 + i);
+        	}
+		    
+        	LibraryModel libraryModel = new LibraryModel(null, Main.resource.getResourceLocal("testLibraryModel"), null);
+        	
+        	libraryModel.filterBy( (BAudio audio) -> audio.<Integer>getMetadata("test_meta") > 1234 );
+        	libraryModel.sortBy( (BAudio a0, BAudio a1) -> a1.<Integer>getMetadata("test_meta") - a0.<Integer>getMetadata("test_meta") );
+        	
+        	Iterator<BAudio> audioIterator = libraryModel.iterator();
+        	while (audioIterator.hasNext()) {
+        		Main.log.log(audioIterator.next().<Integer>getMetadata("test_meta"));
+        	}
         }
     }
 
--- src/com/bateleur/app/model/LibraryModel.java
+++ src/com/bateleur/app/model/LibraryModel.java
@@ -1,40 +1,59 @@
 package com.bateleur.app.model;
 
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.Iterator;
-import java.util.function.Function;
+import java.util.List;
+import java.util.function.Predicate;
 
 import com.bateleur.app.datatype.BAudio;
+import com.bateleur.app.datatype.BAudioFile;
 import com.therealergo.main.resource.ResourceFile;
 import com.therealergo.main.resource.ResourceFolder;
 
 public class LibraryModel implements Iterable<BAudio> {
-	public LibraryModel (ResourceFile file, ResourceFolder data) {
-
+	private List<BAudio> listLibarary;
+	private List<BAudio> listFiltered;
+	
+	public LibraryModel(ResourceFile file, ResourceFolder data, SettingsModel settings) throws IOException {
+		listLibarary = new ArrayList<BAudio>();
+		listFiltered = new ArrayList<BAudio>();
+		
+		ResourceFile[] audioFileList = data.listFileChildren();
+		for (int i = 0; i<audioFileList.length; i++) {
+			listLibarary.add(new BAudioFile(audioFileList[i]));
+		}
+		
+		reset();
 	}
-
-	public void update (SettingsModel settings) {
-
+	
+	/**
+	 * TODO: Not yet implemented
+	 * @param settings
+	 */
+	public void update(SettingsModel settings) {
 	}
-
+	
 	public void sortBy(Comparator<BAudio> comparator) {
-
+		listFiltered.sort(comparator);
 	}
 
-	public void filterBy(Function<BAudio, Boolean> filter) {
-
-	}
-
-	public int size() {
-
+	public void filterBy(Predicate<BAudio> filter) {
+		listFiltered.removeIf(filter.negate());
 	}
 
 	public void reset() {
+		listFiltered.clear();
+		listFiltered.addAll(listLibarary);
+	}
 
+	public int size() {
+		return listFiltered.size();
 	}
 
 	@Override
 	public Iterator<BAudio> iterator() {
-		return null;
+		return listFiltered.iterator();
 	}
 }
