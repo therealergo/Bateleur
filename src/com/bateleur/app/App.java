89c1aec52bc789a7cd8b94d8cb017faf427c0d8f
Emilio Lopez <eil11@case.edu>
2019-03-26 14:56:58 -0400

    Change loading of view.fxml so that it runs on Emilio's machine

--- src/com/bateleur/app/App.java
+++ src/com/bateleur/app/App.java
@@ -33,56 +33,56 @@ public class App extends Application {
     	LibraryModel  library  = new LibraryModel (settings, Main.resource.getResourceFolderClass("library", App.class));
 //    	PlaylistModel playlist = new PlaylistModel(settings);
     	QueueModel    queue    = new QueueModel   (settings);
-    	
+
     	{ // Test of SettingsModel
 		    Main.log.log(settings.get(settings.TEST_VAL));
 		                 settings.set(settings.TEST_VAL.to((int)(System.nanoTime()%8)) );
 		    Main.log.log(settings.get(settings.TEST_VAL));
     	}
-    	
+
         { // Test of BAudio
         	BAudio audio = new BAudioLocal(settings, Main.resource.getResourceFileLocal("testBAudio>meta_test_file"));
-		    
+
 		    Integer test_meta0 = audio.get(settings.TEST_VAL);
 		    Main.log.log(test_meta0);
-		    
+
 		    audio.set(settings.TEST_VAL.to(123));
-		    
+
 		    Integer test_meta1 = audio.get(settings.TEST_VAL);
 		    Main.log.log(test_meta1);
         }
-        
+
         { // Test of LibraryModel
         	for (int i = 0; i<8; i++) {
             	BAudio audio = new BAudioLocal(settings, Main.resource.getResourceFileLocal("testLibraryModel>meta_test_" + i));
     		    audio.set(settings.TEST_VAL.to(1230+i));
         	}
-		    
+
         	LibraryModel libraryTest = new LibraryModel(settings, Main.resource.getResourceFolderLocal("testLibraryModel"));
-        	
+
         	libraryTest.filterBy( (BAudio audio) -> audio.get(settings.TEST_VAL) > 1234 );
         	libraryTest.sortBy( (BAudio a0, BAudio a1) -> a1.get(settings.TEST_VAL) - a0.get(settings.TEST_VAL) );
-        	
+
         	Iterator<BAudio> audioIterator = libraryTest.iterator();
         	while (audioIterator.hasNext()) {
         		Main.log.log(audioIterator.next().get(settings.TEST_VAL));
         	}
         }
-        
+
         { // Test of PlaybackModel
         	BAudio audio = new BAudioLocal(settings, Main.resource.getResourceFileLocal("testPlaybackModel>__meta_test"), Main.resource.getResourceFileLocal("testPlaybackModel>test.mp3").getPath().toUri());
         	playback.loadAudio(audio, 0);
         	playback.play(0);
         }
-        
+
         library.update();
         library.sortBy( (BAudio audio1, BAudio audio2) -> audio2.get(settings.AUDIO_PROP_TITLE).compareTo(audio1.get(settings.AUDIO_PROP_TITLE)) );
         playback.loadAudio(library.iterator().next(), 0);
         playback.play(0);
         queue.setQueue(library, library.iterator().next());
-        
+
         { // Start FXML window
-	    	FXMLLoader loader = new FXMLLoader(getClass().getResource("/resources/gui/views/sample.fxml"));
+			FXMLLoader loader = new FXMLLoader(Main.resource.getResourceFileClass("view.fxml", App.class).getPath().toUri().toURL());
 	    	BuilderFactory defaultBuilderFactory = new JavaFXBuilderFactory();
 	    	loader.setBuilderFactory(b -> {
 	    		switch (b.getSimpleName()) {
@@ -111,21 +111,21 @@ public class App extends Application {
 
 	        primaryStage.setTitle("Bateleur INDEV");
 	        ((Label)root.lookup("#topBarLabel")).setText(primaryStage.getTitle());
-	        
+
 	        scene.setMoveControl(root.lookup("#topBarDrag"));
-	        
+
 	        root.lookup("#topBarMinimize").setOnMousePressed(new EventHandler<MouseEvent>(){
 	        	@Override public void handle(MouseEvent event) {
 					scene.minimise();
 				}
 	        });
-	        
+
 	        root.lookup("#topBarMaximize").setOnMousePressed(new EventHandler<MouseEvent>(){
 	        	@Override public void handle(MouseEvent event) {
 					scene.maximise();
 				}
 	        });
-	        
+
 	        root.lookup("#topBarClose").setOnMousePressed(new EventHandler<MouseEvent>(){
 	        	@Override public void handle(MouseEvent event) {
 	        		scene.getWindow().hide();
