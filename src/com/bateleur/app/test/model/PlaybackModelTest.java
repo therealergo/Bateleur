9d60f42e79ec07deb583b4f323025afe90c3b254
Emilio Lopez <eil11@case.edu>
2019-03-28 18:14:47 -0400

    add test for loading different audio

--- src/com/bateleur/app/test/model/PlaybackModelTest.java
+++ src/com/bateleur/app/test/model/PlaybackModelTest.java
@@ -19,6 +19,9 @@ import static org.mockito.ArgumentMatchers.any;
 @RunWith(JfxRunner.class)
 public class PlaybackModelTest {
 
+    private static final int FADE_TIME = 0;
+    private static final long DELAY_TIME_MS = 3000L;
+
     private PlaybackModel playbackModel;
 
     private static SettingsModel settings;
@@ -26,9 +29,7 @@ public class PlaybackModelTest {
     @BeforeClass
     public static void setupClass() throws Exception {
         Main.mainInit(App.class, new String[]{});
-
-
-        Thread.sleep(5000L);    // wait for Main to init correctly
+        Thread.sleep(DELAY_TIME_MS);    // wait for Main to init correctly
 
         settings = new SettingsModel(Main.resource.getResourceFileClass("settings.ser", App.class));
     }
@@ -45,15 +46,34 @@ public class PlaybackModelTest {
                                        Main.resource.getResourceFileLocal("testPlaybackModel>__meta_test"),
                                        Main.resource.getResourceFileLocal("testPlaybackModel>test.mp3").getPath().toUri());
 
-        //when
-        playbackModel.loadAudio(audio, 0);
+        // When
+        playbackModel.loadAudio(audio, FADE_TIME);
 
-        // then
+        // Then
         assertTrue(playbackModel.isAudioLoaded());
         assertEquals(audio, playbackModel.getLoadedAudio());
     }
 
+    public void test_loadedAudio_loadAudio_loadsNewAudio() throws Exception {
+        // Given
+        BAudio originalAudio = new BAudioLocal(settings,
+                                               Main.resource.getResourceFileLocal("testPlaybackModel>__meta_test"),
+                                               Main.resource.getResourceFileLocal("testPlaybackModel>test.mp3").getPath().toUri());
 
+        BAudio audio = new BAudioLocal(settings,
+                                       Main.resource.getResourceFileLocal("testPlaybackModel>__meta_test"),
+                                       Main.resource.getResourceFileLocal("testPlaybackModel>0.mp3").getPath().toUri());
+        assert !(audio.equals(originalAudio));    // validates the test environment was created correctly, not unit
+
+        playbackModel.loadAudio(originalAudio, FADE_TIME);
+        playbackModel.play(FADE_TIME);
+
+        // When
+        playbackModel.loadAudio(audio, FADE_TIME);
+        // Then
+        assertTrue(playbackModel.isAudioLoaded());
+        assertEquals(audio, playbackModel.getLoadedAudio());
+    }
 
 
 }
