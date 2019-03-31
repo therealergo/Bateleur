bd1c8fa1bc82e546fa35e5c5867dfb4ef7bdf8e8
Emilio Lopez <eil11@case.edu>
2019-03-31 12:54:54 -0400

    beginning queuemodel testing

--- src/com/bateleur/app/test/model/QueueModelTest.java
+++ src/com/bateleur/app/test/model/QueueModelTest.java
@@ -10,7 +10,10 @@ import com.therealergo.main.Main;
 import de.saxsys.javafx.test.JfxRunner;
 import org.junit.Before;
 import org.junit.BeforeClass;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import static junit.framework.TestCase.*;
 
 @RunWith(JfxRunner.class)
 public class QueueModelTest {
@@ -28,13 +31,12 @@ public class QueueModelTest {
     public static void setupClass() throws Exception {
         Main.mainInit(App.class, new String[]{});
         Thread.sleep(DELAY_TIME_MS);    // wait for Main to init correctly
+        settings = new SettingsModel(Main.resource.getResourceFileClass("settings.ser", App.class));
 
         for (int i = 0; i<8; i++) {
             BAudio audio = new BAudioLocal(settings, Main.resource.getResourceFileLocal("testLibraryModel>meta_test_" + i));
-            audio.set(settings.TEST_VAL.to(1231+i));
+            audio.set(settings.TEST_VAL.to(1230+i));
         }
-
-        settings = new SettingsModel(Main.resource.getResourceFileClass("settings.ser", App.class));
         library = new LibraryModel(settings, Main.resource.getResourceFolderLocal("testLibraryModel"));
     }
 
@@ -44,13 +46,45 @@ public class QueueModelTest {
     @Before
     public void setup() throws Exception {
         queueModel = new QueueModel(settings);
-
         testAudio = new BAudioLocal(settings,
                                     Main.resource.getResourceFileLocal("testPlaybackModel>__meta_test"),
                                     Main.resource.getResourceFileLocal("testPlaybackModel>test.mp3").getPath().toUri());
 
 
-        queueModel.setQueue(library, testAudio);
+        queueModel.setQueue(library, library.iterator().next());
+    }
+
+    @Test
+    public void test_firstAudioQueueEn_skipForwards_nextSong() {
+        // Given
+        queueModel.setQueueState(true);
+        BAudio startingAudio = queueModel.get();
+
+        // When
+        queueModel.skipForwards();
+        BAudio nextAudio = queueModel.get();
+
+        // Then
+        assertNotNull(nextAudio);
+        assertNotSame(startingAudio, nextAudio);
+        assertNotSame(nextAudio, testAudio);
+    }
+
+    @Test
+    public void test_firstAudioQueueEn_skipBackwards_lastSong() {
+        // Given
+        queueModel.setQueueState(true);
+        BAudio startingAudio = queueModel.get();
+
+        // When
+        queueModel.skipBackwards();
+        BAudio prevAudio = queueModel.get();
+
+
+        // Then
+        assertNotNull(prevAudio);
+        assertNotSame(startingAudio, prevAudio);
+        assertNotSame(prevAudio, testAudio);
     }
 
 
