de91007c0b05263aa73f7d837d4dea27f4133f92
therealergo <therealergo@gmail.com>
2019-04-08 02:11:02 -0400

    Update/Document SettingsController stub

--- src/com/bateleur/app/controller/SettingsController.java
+++ src/com/bateleur/app/controller/SettingsController.java
@@ -1,13 +1,27 @@
 package com.bateleur.app.controller;
 
-import com.bateleur.app.model.SettingsModel;
-
 import java.io.Serializable;
 
-public class SettingsController {
-    public SettingsController(SettingsModel settings) {
-    }
+import com.bateleur.app.datatype.BFile;
 
-    public <T extends Serializable> void onSettingChanged (String key, Class<T> type, T value) {
-    }
+public class SettingsController {
+	/** Reference to this SettingsController's MasterController. */
+	public MasterController master;
+
+	/**
+	 * Perform any initialization required by this SettingsController.
+	 * Should only be called by the MasterController when the master is ready and this SettingsController is to be initialized.
+	 * @param master This SettingsController's master controller.
+	 */
+	public void initialize(MasterController master) {
+		this.master = master;
+	}
+
+	/**
+	 * Callback triggered whenever a setting's value is adjusted by the user.
+	 * @param setting The setting whose value to adjust.
+	 * @param value   The value to which the setting is to be set.
+	 */
+	public <T extends Serializable> void onSettingChanged(BFile.Entry<T> setting, T value) {
+	}
 }
